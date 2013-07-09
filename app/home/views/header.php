bbe444205f2d570a19c9154b8bd70d1ef152043c
yanue <yansueh@gmail.com>
2013-07-09 13:54:12 +0800

    修改核心i架构

---
+++ app/home/views/header.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * header.php
+ * 
+ * @copyright	http://yanue.net/
+ * @author 		yanue <yanue@outlook.com>
+ * @version		1.0.0 - 2013-07-08
+ */
---
+++ app/home/views/layout1.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <title>哈哈哈 - perfect</title>
+</head>
+<body>
+
+<?php
+Debug::dump($this);
+
+echo $a;
+echo $this->aa;
+?>
+
+</body>
+</html>
